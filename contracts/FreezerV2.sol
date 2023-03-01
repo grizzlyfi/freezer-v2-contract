a898f8d23a800375bbb4ce787f30c1a85a43015b
Roger Staubli <roger@staubli-software-solutions.ch>
2023-03-01 17:45:03 +0100

    add staking pool reward functions to freezer

--- contracts/FreezerV2.sol
+++ contracts/FreezerV2.sol
@@ -1,4 +1,51 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-contract FreezerV2 {}
+import "./Interfaces/IStakingPool.sol";
+
+contract FreezerV2 {
+    struct participantData {
+        uint256 deposited;
+        uint256 ghny;
+    }
+
+    uint256 constant DECIMAL_OFFSET = 1 ether;
+
+    IStakingPool public StakingPool;
+    uint256 honeyRoundMask;
+    uint256 bnbRoundMask;
+    uint256 totalFreezedAmount;
+
+    function freeze(uint256 amount, address referral) external {}
+
+    function unfreeze() external {}
+
+    function _claimAllStakingRewards() internal {
+        uint256 totalStakingIncrease = StakingPool.balanceOf(address(this)) -
+            totalFreezedAmount;
+        uint256 totalLpRewards = StakingPool.lpBalanceOf(address(this));
+        uint256 additionalHoney = StakingPool.getPendingHoneyRewards();
+
+        if (totalStakingIncrease > 0) {
+            StakingPool.unstake(totalStakingIncrease);
+        }
+        (uint256 claimedAdditionalHoney, uint256 claimedBnb) = StakingPool
+            .claimLpTokens(totalLpRewards, additionalHoney, address(this));
+        if (totalStakingIncrease + claimedAdditionalHoney > 0) {
+            _rewardHoney(totalStakingIncrease + claimedAdditionalHoney);
+        }
+        if (claimedBnb > 0) {
+            _rewardBnb(claimedBnb);
+        }
+    }
+
+    function _rewardHoney(uint256 amount) internal {
+        require(totalFreezedAmount > 0, "total freezed amount is 0");
+        honeyRoundMask += (DECIMAL_OFFSET * amount) / totalFreezedAmount;
+    }
+
+    function _rewardBnb(uint256 amount) internal {
+        require(totalFreezedAmount > 0, "total freezed amount is 0");
+        bnbRoundMask += (DECIMAL_OFFSET * amount) / totalFreezedAmount;
+    }
+}
---
+++ contracts/Interfaces/IStakingPool.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+interface IStakingPool {
+    function balanceOf(address from) external view returns (uint256);
+
+    function lpBalanceOf(address from) external view returns (uint256);
+
+    function getPendingHoneyRewards() external view returns (uint256);
+
+    function unstake(uint256 amount) external;
+
+    function claimLpTokens(
+        uint256 lpRewards,
+        uint256 honeyRewards,
+        address to
+    ) external returns (uint256 additionalGhny, uint256 additionalBnb);
+}
